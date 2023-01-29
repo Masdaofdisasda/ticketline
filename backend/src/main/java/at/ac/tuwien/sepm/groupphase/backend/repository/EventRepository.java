c801aaadcef5d3fda81350234c4ec56b299d81cb
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2023-01-29 22:23:35 +0100

    [#99]: fixed topOfMonth Query

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
@@ -51,7 +51,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
    */
   @Query("SELECT pf.event FROM Performance pf"
     + " LEFT JOIN Ticket t ON t.performance = pf"
-    + " WHERE t.booking IS NOT NULL AND MONTH(t.booking.createdDate) = MONTH(CURRENT DATE)"
+    + " WHERE t.booking IS NOT NULL AND MONTH(t.booking.createdDate) = MONTH(CURRENT_DATE)"
     + " GROUP BY pf.event"
     + " ORDER BY COUNT(COALESCE(t.booking, 0)) DESC, pf.event.startDate")
   Page<Event> findTopOfMonth(Pageable pageable);
